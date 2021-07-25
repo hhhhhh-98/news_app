cd759d9ad9812223a50a2c65b2f56c4f4efc49aa
hh <2408138772@qq.com>
2021-07-25 18:38:59 +0800

    个人页面

--- app/app_news/globle.js
+++ app/app_news/globle.js
@@ -1,3 +1,25 @@
 export default {
   app_key:"b64be5563887bfa08438a814cf90adc4",
+  
+  
+  //方法写在下面 注释写上方法的作用和参数的意义
+  
+  /*
+  **
+  * 用于判断用户是否登录，
+  * @url 登录成功跳转的url
+  * @store.state.login 登录状态
+  */
+  judgeLogin : function (url){
+	 if(this.$store.state.login === true)
+		return true;
+	else{
+		this.$store.state.backurl = url;
+		uni.navigateTo({
+		    url: '/pages/login/login'  
+		});
+	}
+  }
+  
+  
 }
